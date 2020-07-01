6d76913d01c9f630020cf5f96524b91502c540ce
Dilshan Jayasanka <dilshan.jayasanka88@gmail.com>
2020-07-01 16:39:45 +0530

    added keditor
    keditor with bs 3.7 working project

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,16 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" type="text/css"
-        href="{{ asset('assets/plugins/dep/bootstrap-3.3.6/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" type="text/css"
-        href="{{ asset('assets/plugins/dep/font-awesome-4.5.0/css/font-awesome.min.css') }}" />
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/dep/bootstrap-3.4.1/css/bootstrap.min.css') }}" data-type="keditor-style" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/dep/font-awesome-4.7.0/css/font-awesome.min.css') }}" data-type="keditor-style" />
     <!-- Start of KEditor styles -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/editor-1.7/css/keditor.min.css') }}" />
-    <link rel="stylesheet" type="text/css"
-        href="{{ asset('assets/plugins/editor-1.7/css/keditor-components.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/editor-2.0.1/css/keditor.min.css') }}" data-type="keditor-style" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/editor-2.0.1/css/keditor-components.min.css') }}" data-type="keditor-style" />
     <!-- End of KEditor styles -->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/dep/code-prettify/src/prettify.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/editor-2.0.1/css/examples.css') }}" />
+
+
     @yield('styles')
     <title>Document</title>
 </head>
@@ -24,21 +25,26 @@
 
     @yield('content')
 
+
+
+
     <script type="text/javascript" src="{{ asset('assets/plugins/dep/jquery-1.11.3/jquery-1.11.3.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/plugins/dep/bootstrap-3.3.6/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/plugins/dep/bootstrap-3.4.1/js/bootstrap.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/plugins/dep/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/plugins/dep/jquery.nicescroll-3.6.6/jquery.nicescroll.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/plugins/dep/ckeditor-4.5.6/ckeditor.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/plugins/dep/ckeditor-4.5.6/adapters/jquery.js') }}"></script>
+{{--    {{ asset('assets/plugins/dep/jquery.nicescroll-3.6.6/jquery.nicescroll.min.js') }}--}}
+
+    <script type="text/javascript" src="{{ asset('assets/plugins/dep/ckeditor-4.11.4/ckeditor.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/plugins/dep/formBuilder-2.5.3/form-builder.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/plugins/dep/formBuilder-2.5.3/form-render.min.js') }}"></script>
     <!-- Start of KEditor scripts -->
-    <script type="text/javascript" src="{{ asset('assets/plugins/editor-1.7/js/keditor.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/plugins/editor-1.7/js/keditor-components.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/plugins/editor-2.0.1/js/keditor.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/plugins/editor-2.0.1/js/keditor-components.min.js') }}"></script>
     <!-- End of KEditor scripts -->
     <script type="text/javascript" src="{{ asset('assets/plugins/dep/code-prettify/src/prettify.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/plugins/dep/js-beautify-1.7.5/js/lib/beautify.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/plugins/dep/js-beautify-1.7.5/js/lib/beautify-html.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/plugins/editor-2.0.1/js/examples.js') }}"></script>
+
     @yield('scripts')
     @yield('script')
 
--- resources/views/pages/new.blade.php
+++ resources/views/pages/new.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('styles')
-    
+
 @endsection
 
 @section('content')
@@ -12,7 +12,7 @@
     </div>
     <div class="row">
         <div class="col-lg-12">
-            <div data-keditor="html">
+            <div data-keditor="html" style="height: 600px;">
                 <div id="content-area"></div>
             </div>
         </div>
@@ -21,14 +21,72 @@
 @endsection
 
 @section('scripts')
-    
+
 @endsection
 
 @section('script')
 <script type="text/javascript" data-keditor="script">
     $(function () {
         $('#content-area').keditor({
-            snippetsUrl:"{{ asset('assets/plugins/editor-1.7/snippets/snippets.blade.html') }}"
+            title:'New Page',
+            // testing configs
+            onReady:function(){
+                var backBtn = $("<a href='/home' class='keditor-ui keditor-topbar-btn'><i class='fa fa-arrow-left' style='color:#ff5e00' title='Back to Pages'></i></a>");
+                $(this.topbar[0]).find('.keditor-ui.keditor-topbar-right').prepend(backBtn);
+                $(this.topbar[0]).find('.keditor-ui.keditor-topbar-right  i.fa-save').css({color:'#199c09'});
+            },
+            onSave:function(t){
+                console.log(t);
+            },
+            // END testing configs
+            snippetsUrl: "{{ asset('assets/plugins/editor-2.0.1/snippets/snippets.blade.html') }}",
+            containerSettingEnabled: true,
+            containerSettingInitFunction: function (form, keditor) {
+                // Add control for settings form
+                form.append(
+                    '<div class="form-horizontal">' +
+                    '   <div class="form-group">' +
+                    '       <div class="col-sm-12">' +
+                    '           <label>Background color</label>' +
+                    '           <input type="text" class="form-control txt-bg-color" />' +
+                    '       </div>' +
+                    '   </div>' +
+                    '</div>'
+                );
+
+                // Add event handle for background color textbox
+                form.find('.txt-bg-color').on('change', function () {
+                    // Get current setting container
+                    var container = keditor.getSettingContainer();
+                    // Find '.row' for setting background color
+                    // Note: Make sure you have a div for setting background color
+                    var row = container.find('.row');
+                    if (container.hasClass('keditor-sub-container')) {
+                        // Do nothing
+                    } else {
+                        row = row.filter(function () {
+                            return $(this).parents('.keditor-container').length === 1;
+                        });
+                    }
+
+                    // Set background color with value of textbox
+                    // row.css('background-color', this.value);
+                    container.css('background-color', this.value);
+                });
+            },
+            containerSettingShowFunction: function (form, container, keditor) {
+                // Find '.row' div
+                // Note: Make sure you have a div for setting background color
+                var row = container.find('.row');
+                // User "prop()" method to get properties of HTML element
+                var backgroundColor = row.prop('style').backgroundColor || '';
+                // User 'backgroundColor' for value of background color textbox
+                form.find('.txt-bg-color').val(backgroundColor);
+            },
+            containerSettingHideFunction: function (form, keditor) {
+                // Clean value of background color textbox when hiding settings form
+                form.find('.txt-bg-color').val('');
+            }
         });
     });
 </script>
